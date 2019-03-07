6b4b1bc328a0cd087bab280edc0c9e60bd549e3c
RikoLi <939958083@qq.com>
2019-03-08 03:14:31 +0800

    add map label

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 // Initialize Amap
 let map = new AMap.Map('container', {
     resizeEnable: true,
-    mapStyle: 'amap://styles/macaron',
-    zoom: 18,
+    mapStyle: 'amap://styles/normal',
+    zoom: 15,
     viewMode: '3D'
 });
 // Set map display features
 map.setFeatures(['road','point', 'bg']);
+map.setStatus({dragEnable: false, zoomEnable: false}); //Disenable dragging, zooming
 
 // Apply geolocation via browser
 map.plugin('AMap.Geolocation', () => {
@@ -18,15 +19,15 @@ map.plugin('AMap.Geolocation', () => {
         // 定位按钮的停靠位置的偏移量，默认：Pixel(10, 20)
         buttonOffset: new AMap.Pixel(10, 20),
         //  定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
-        zoomToAccuracy: true,     
-        //  定位按钮的排放位置,  RB表示右下
-        buttonPosition: 'RB'
+        zoomToAccuracy: false,     
+        // 不显示定位按钮
+        showButton: false
     });
     map.addControl(geolocation);
     geolocation.getCurrentPosition();
 });
 
-// time test
+// Time display
 setInterval(() => {
     let date = new Date();
     sec = date.getSeconds().toString();
@@ -34,3 +35,60 @@ setInterval(() => {
     hour = date.getHours().toString();
     document.getElementById('time-p').innerHTML = hour + ':' + min + ':' + sec;
 }, 1000);
+
+// Button click events
+let openStatus = () => {
+    button = document.getElementById('status-button');
+    open('status.html', '_self');
+}
+let openItem = () => {
+    button = document.getElementById('item-button');
+    open('item.html', '_self');
+}
+let openCam = () => {
+    button = document.getElementById('camera-button');
+    open('camera.html', '_self');
+}
+
+/**
+* 地图交互
+* 用于用户点击地图上可交互对象时进行交互
+*/
+AMapUI.loadUI(['misc/PositionPicker'], (PositionPicker) => {
+    let positionPicker = new PositionPicker({
+        mode:'dragMap',//设定为拖拽地图模式，可选'dragMap'、'dragMarker'，默认为'dragMap'
+        map: map,//依赖地图对象
+        iconStyle: {
+            url: 'https://webapi.amap.com/theme/v1.3/markers/n/loc.png',
+            size: [23, 23],
+            ancher: [11.5, 11.5]
+        }
+    });
+    //TODO:事件绑定、结果处理等
+    let oldMarkerArray = [];
+    positionPicker.on('success', (positionResult) => {
+        let poiArray = positionResult.regeocode.pois;
+        let locationArray = [];
+        for (let i = 0; i < 7; i++) {
+            locationArray.push([poiArray[i].location.lng, poiArray[i].location.lat]);
+        }
+        // Create markers for POIs
+        map.remove(oldMarkerArray);
+        oldMarkerArray = [];
+        for (let i = 0; i < 7; i++) {
+            let marker = new AMap.Marker({
+                position: locationArray[i],
+                label: {content: poiArray[i].name, offset: new AMap.Pixel(10, 30)}
+            });
+            map.add(marker);
+            oldMarkerArray.push(marker);
+        }
+    });
+    positionPicker.on('fail', (positionResult) => {
+        // 海上或海外无法获得地址信息
+        alert('Get positions information failed!');
+    });
+    positionPicker.start();
+});
+
+
