f87d86a3d863bfe09c8639e4f8a6eda6ded56df9
RikoLi <939958083@qq.com>
2019-03-08 17:05:49 +0800

    add POI info display

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ let map = new AMap.Map('container', {
     viewMode: '3D'
 });
 // Set map display features
-map.setFeatures(['road','point', 'bg']);
+map.setFeatures(['road', 'point', 'bg']);
 map.setStatus({dragEnable: false, zoomEnable: false}); //Disenable dragging, zooming
 
 // Apply geolocation via browser
@@ -23,8 +23,12 @@ map.plugin('AMap.Geolocation', () => {
         // 不显示定位按钮
         showButton: false
     });
+    let position = [];
     map.addControl(geolocation);
     geolocation.getCurrentPosition();
+    AMap.event.addListener(geolocation, 'complete', (data) => {
+        document.getElementById('current-pos').innerHTML = '当前位置：'+'['+data.position.lng+', '+data.position.lat+']';
+    });
 });
 
 // Time display
@@ -60,8 +64,8 @@ AMapUI.loadUI(['misc/PositionPicker'], (PositionPicker) => {
         map: map,//依赖地图对象
         iconStyle: {
             url: 'https://webapi.amap.com/theme/v1.3/markers/n/loc.png',
-            size: [23, 23],
-            ancher: [11.5, 11.5]
+            size: [24, 24],
+            ancher: [12, 12]
         }
     });
     //TODO:事件绑定、结果处理等
@@ -69,26 +73,50 @@ AMapUI.loadUI(['misc/PositionPicker'], (PositionPicker) => {
     positionPicker.on('success', (positionResult) => {
         let poiArray = positionResult.regeocode.pois;
         let locationArray = [];
-        for (let i = 0; i < 7; i++) {
+        for (let i = 0; i < 5; i++) {
             locationArray.push([poiArray[i].location.lng, poiArray[i].location.lat]);
         }
         // Create markers for POIs
         map.remove(oldMarkerArray);
         oldMarkerArray = [];
-        for (let i = 0; i < 7; i++) {
+        for (let i = 0; i < 5; i++) {
             let marker = new AMap.Marker({
                 position: locationArray[i],
-                label: {content: poiArray[i].name, offset: new AMap.Pixel(10, 30)}
+                title: poiArray[i].name
             });
             map.add(marker);
             oldMarkerArray.push(marker);
         }
-    });
-    positionPicker.on('fail', (positionResult) => {
-        // 海上或海外无法获得地址信息
-        alert('Get positions information failed!');
+        // POI touching listening
+        for (let i = 0; i < 5; i++) {
+            oldMarkerArray[i].on('click', (marker) => {
+                // Calculate distance
+                let targetPos = [marker.lnglat.lng, marker.lnglat.lat];
+                let selfPos = document.getElementById('current-pos').innerHTML;
+                selfPos = selfPos.split('：');
+                selfPos = selfPos[1].replace('[', '');
+                selfPos = selfPos.replace(']', '');
+                selfPos = selfPos.split(', ');
+                selfPos = [Number(selfPos[0]), Number(selfPos[1])];
+                document.getElementById('dist-p').innerHTML = AMap.GeometryUtil.distance(targetPos, selfPos).toFixed(2) + 'm';
+                // Print POI name
+                document.getElementById('target-name').innerHTML = marker.target.D.title;
+            });
+        }
     });
     positionPicker.start();
 });
+
+
+/**
+* 本地数据存储
+*/
+
+
+
+
+/**
+* 相机+方向传感器定位AR显示
+*/
 
 
