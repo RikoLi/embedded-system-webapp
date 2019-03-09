9af95eca78caac94693934940d368752026fe478
RikoLi <939958083@qq.com>
2019-03-09 11:23:21 +0800

    update position getting method for PC and mobile

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 let map = new AMap.Map('container', {
     resizeEnable: true,
     mapStyle: 'amap://styles/normal',
-    zoom: 15,
+    zoom: 16,
     viewMode: '3D'
 });
 // Set map display features
@@ -14,18 +14,19 @@ map.plugin('AMap.Geolocation', () => {
     let geolocation = new AMap.Geolocation({
         // 是否使用高精度定位，默认：true
         enableHighAccuracy: true,
-        // 设置定位超时时间，默认：无穷大
-        timeout: 10000,
         // 定位按钮的停靠位置的偏移量，默认：Pixel(10, 20)
         buttonOffset: new AMap.Pixel(10, 20),
         //  定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
         zoomToAccuracy: false,     
         // 不显示定位按钮
-        showButton: false
+        showButton: false,
+        maximumAge: 10
     });
     let position = [];
     map.addControl(geolocation);
-    geolocation.getCurrentPosition();
+    if (window.navigator.userAgent === 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36')
+        geolocation.getCurrentPosition();//For PC users
+    else geolocation.watchPosition();    //For mobile users
     AMap.event.addListener(geolocation, 'complete', (data) => {
         document.getElementById('current-pos').innerHTML = '当前位置：'+'['+data.position.lng+', '+data.position.lat+']';
     });
@@ -63,9 +64,9 @@ AMapUI.loadUI(['misc/PositionPicker'], (PositionPicker) => {
         mode:'dragMap',//设定为拖拽地图模式，可选'dragMap'、'dragMarker'，默认为'dragMap'
         map: map,//依赖地图对象
         iconStyle: {
-            url: 'https://webapi.amap.com/theme/v1.3/markers/n/loc.png',
-            size: [24, 24],
-            ancher: [12, 12]
+            url: 'https://gss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/zhidao/wh%3D600%2C800/sign=4d397db9af51f3dec3e7b162a4dedc27/500fd9f9d72a6059f50254692834349b033bbab7.jpg',
+            size: [2, 2],
+            ancher: [1, 1]
         }
     });
     //TODO:事件绑定、结果处理等
@@ -86,10 +87,8 @@ AMapUI.loadUI(['misc/PositionPicker'], (PositionPicker) => {
             });
             map.add(marker);
             oldMarkerArray.push(marker);
-        }
-        // POI touching listening
-        for (let i = 0; i < 5; i++) {
-            oldMarkerArray[i].on('touchend', (marker) => {
+            // POI touching listening
+            oldMarkerArray[i].on('touchstart', (marker) => {
                 // Calculate distance
                 let targetPos = [marker.lnglat.lng, marker.lnglat.lat];
                 let selfPos = document.getElementById('current-pos').innerHTML;
@@ -102,6 +101,8 @@ AMapUI.loadUI(['misc/PositionPicker'], (PositionPicker) => {
                 // Print POI name
                 document.getElementById('target-name').innerHTML = marker.target.D.title;
             });
+        }
+        for (let i = 0; i < 5; i++) {
         }
     });
     positionPicker.start();
