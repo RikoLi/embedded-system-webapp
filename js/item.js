198deb413f3bbe606a77f3c45496181fd93eeec0
RikoLi <939958083@qq.com>
2019-03-26 08:46:39 +0800

    Add item display

--- js/item.js
+++ js/item.js
@@ -1,3 +1,19 @@
 function returnHome() {
     window.open('index.html', '__self');
 }
+
+function updateItemList() {
+    let bagArray = JSON.parse(localStorage.getItem('bag'));
+    for (let i = 0; i < 3; i ++) {
+        let itemName = bagArray[i].name;
+        let itemNum = bagArray[i].number;
+        let slcArea = document.getElementById('item-selection');
+        let newOpt = document.createElement('option')
+        let optText = document.createTextNode(itemName);
+        
+        newOpt.appendChild(optText);
+        slcArea.appendChild(newOpt);
+    }
+}
+
+updateItemList();
